a21e57f0361758f14198596797c539f63a979601
Louis_45 <github@louis45.de>
2024-04-28 17:02:48 +0200

    Improved stability for running inside docker and improved api usage

--- run.py
+++ run.py
@@ -3,15 +3,21 @@ import requests
 import logging
 import os
 
-apikey = os.environ['APIKEY']
-delay = int(os.environ['DELAY'])
+apikey, delay = os.environ['APIKEY'], int(os.environ['DELAY'])
+last_ip = ""
 
 while True:
     ip = requests.get("https://api.ipify.org").text
-    response = requests.post("https://proxy.webshare.io/api/proxy/config/",
-                             json={"authorized_ips": [ip]},
-                             headers={"Authorization": apikey})
-    logging.warning('Set IP to: ' + ip)
-    del ip
-    del response
+    if ip != last_ip:
+        response = requests.post("https://proxy.webshare.io/api/proxy/config/",
+                                json={"authorized_ips": [ip]},
+                                headers={"Authorization": apikey})
+        if response.status_code == 200:
+            if response.json()["authorized_ips"][0] == ip:
+                logging.warning("Successfully updated ip to: " + ip)
+            else:
+                logging.warning("Ip didn't update to " + ip + " for some reason")
+        else:
+            logging.warning("Received " + response.status_code + " error while updating ip to: " + ip)
+    last_ip = ip
     time.sleep(delay)
